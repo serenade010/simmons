86f86c39f8128bb2250e0b20dedc51840e2b1ed1
serenade010 <lukechu010@gmail.com>
2022-01-05 22:56:17 +0800

    adding space on planning page

--- src/Pages/Planning.js
+++ src/Pages/Planning.js
@@ -7,23 +7,31 @@ function Planning() {
       <div className="planning-title">近8週的物料規劃:</div>
       <div className="planning-container">
         <div className="planning-entry">
-          2/21: 送出成品: 80加州帝皇床墊 40加寬雙人床墊 40標準床墊{' '}
+          2/21: 送出成品: <div className="planning-item">80加州帝皇床墊</div>{' '}
+          <div className="planning-item">40加寬雙人床墊</div>
+          <div className="planning-item">40標準床墊</div>
         </div>
         <div className="planning-entry">
           2/14: 開始組裝 組裝內容: 80加州帝皇床墊 40加寬雙人床墊 40標準床墊{' '}
         </div>
         <div className="planning-entry">
           2/07: 送出成品: 100加州帝皇床墊 50加寬雙人床墊 30標準床墊
-          開始組裝床表、彈簧體、床體 組裝內容: 80加州帝皇床表、彈簧體、床體
-          40加寬雙人床表、彈簧體、床體 40標準床表、彈簧體、床體
+          <div className="planning-item"> 開始組裝床表、彈簧體、床體 </div>
+          組裝內容: 80加州帝皇床表、彈簧體、床體 40加寬雙人床表、彈簧體、床體
+          40標準床表、彈簧體、床體
         </div>
         <div className="planning-entry">
-          1/31: 開始組裝床墊 組裝內容: 100加州帝皇床墊 50加寬雙人床墊 30標準床墊
+          1/31: 開始組裝床墊 組裝內容:{' '}
+          <div className="planning-item">100加州帝皇床墊</div>
+          <div className="planning-item">50加寬雙人床墊</div>
+          <div className="planning-item"> 30標準床墊</div>
           送出訂單 訂單內容: 表布360、彈簧66000、彈簧筒13200、間棉360{' '}
         </div>
         <div className="planning-entry">
           1/24: 開始組裝床表、彈簧體、床體 組裝內容:
-          100加州帝皇床表、彈簧體、床體 50加寬雙人床表、彈簧體、床體
+          <div className="planning-item">
+            100加州帝皇床表、彈簧體、床體 50加寬雙人床表、彈簧體、床體
+          </div>
           30標準床表、彈簧體、床體 送出訂單 訂單內容: 車花360、拉鍊160、泡棉440{' '}
         </div>
         <div className="planning-entry">
@@ -31,8 +39,7 @@ function Planning() {
           散熱層1280
         </div>
         <div className="planning-entry">
-          1/10: 送出訂單 訂單內容: 車花410、拉鍊210、泡棉510 1/03: 送出訂單
-          訂單內容: 散熱層1500
+          1/10: 送出訂單 訂單內容: 車花410、拉鍊210、泡棉510
         </div>
         <div className="planning-entry">
           1/03: 送出訂單 訂單內容: 散熱層1500
