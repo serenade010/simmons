dfeffd79395d9086dc60260f418fd66db80882b1
serenade010 <lukechu010@gmail.com>
2022-01-17 22:05:01 +0800

    fix view page

--- src/Pages/View.js
+++ src/Pages/View.js
@@ -2,47 +2,70 @@ import { React, useEffect, useState } from 'react';
 import './view.css';
 import 'axios';
 import axios from 'axios';
+import Loader from 'react-loader-spinner';
 
 function View() {
   const [materials, setMaterials] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
+
   const fetchData = async () => {
     axios
       .get('https://simmons-stage-backend.herokuapp.com/inventory')
       .then((response) => {
         setMaterials(response.data['materials']);
         setProducts(response.data['products']);
+        setLoading(false);
       });
   };
   useEffect(() => {
     fetchData();
   }, []);
-  return (
-    <div className="view">
-      <div className="component-view">
-        <div className="view-title">Component View</div>
-        {materials.map((material) => {
-          return (
-            <div className="view-material">
-              <span>{material.material_name}</span>
-              <span>{material.on_hand_balance}</span>
-            </div>
-          );
-        })}
+
+  if (loading) {
+    return (
+      <div className="view">
+        <div className="component-view">
+          <div className="view-title">Component View</div>
+          <div className="loading-container">
+            <Loader type="Oval" color="#777" height={100} width={100} />
+          </div>
+        </div>
+        <div className="product-view">
+          <div className="view-title">Product View</div>
+          <div className="loading-container">
+            <Loader type="Oval" color="#777" height={100} width={100} />
+          </div>
+        </div>
       </div>
-      <div className="product-view">
-        <div className="view-title">Product View</div>
-        {products.map((porducts) => {
-          return (
-            <div className="view-products">
-              <span>{porducts.product_name}</span>
-              <span>{porducts.on_hand_balance}</span>
-            </div>
-          );
-        })}
+    );
+  } else
+    return (
+      <div className="view">
+        <div className="component-view">
+          <div className="view-title">Component View</div>
+          {materials.map((material) => {
+            return (
+              <div className="view-material">
+                <span>{material.material_name}</span>
+                <span>{material.leading_time * 10}</span>
+              </div>
+            );
+          })}
+        </div>
+        <div className="product-view">
+          <div className="view-title">Product View</div>
+          {products.map((porducts) => {
+            return (
+              <div className="view-products">
+                <span>{porducts.product_name}</span>
+                <span>{porducts.on_hand_balance}</span>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default View;
